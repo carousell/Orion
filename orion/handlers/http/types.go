e9a5218fe80aa68de523a4a0a07b33cdf5169ccf
Ankur Shrivastava <ankur@ankurs.com>
2018-08-13 17:36:47 +0800

    Adding support for Websockets to gRPC streams (#73)
    * websockt support first pass
    
    * updating vendor
    
    * adding orion check back
    
    * cancel context if not available

--- orion/handlers/http/types.go
+++ orion/handlers/http/types.go
@@ -13,14 +13,14 @@ import (
 var (
 	//ContentTypeMap is the mapping of content-type with marshaling type
 	ContentTypeMap = map[string]string{
-		"application/json":                modifiers.JSON,
+		ContentTypeJSON:                   modifiers.JSON,
 		"application/jsonpb":              modifiers.JSONPB,
 		"application/x-jsonpb":            modifiers.JSONPB,
 		"application/protobuf":            modifiers.ProtoBuf,
 		"application/proto":               modifiers.ProtoBuf,
 		"application/x-proto":             modifiers.ProtoBuf,
 		"application/vnd.google.protobuf": modifiers.ProtoBuf,
-		"application/octet-stream":        modifiers.ProtoBuf,
+		ContentTypeProto:                  modifiers.ProtoBuf,
 	}
 
 	// DefaultHTTPResponseHeaders are response headers that are whitelisted by default
@@ -32,6 +32,11 @@ var (
 const (
 	//IgnoreNR is the option flag to ignore newrelic for this method
 	IgnoreNR = "IGNORE_NR"
+)
+
+const (
+	ContentTypeJSON  = "application/json"
+	ContentTypeProto = "application/octet-stream"
 )
 
 //HandlerConfig is the configuration for HTTP Handler
@@ -48,17 +53,20 @@ type serviceInfo struct {
 }
 
 type methodInfo struct {
-	svc         *serviceInfo
-	method      handlers.GRPCMethodHandler
-	encoder     handlers.Encoder
-	decoder     handlers.Decoder
-	httpHandler handlers.HTTPHandler
-	httpMethod  []string
-	encoderPath string
-	serviceName string
-	methodName  string
-	urls        []string
-	options     []string
+	svc           *serviceInfo
+	method        handlers.GRPCMethodHandler
+	stream        grpc.StreamHandler
+	encoder       handlers.Encoder
+	decoder       handlers.Decoder
+	httpHandler   handlers.HTTPHandler
+	httpMethod    []string
+	encoderPath   string
+	serviceName   string
+	methodName    string
+	urls          []string
+	options       []string
+	clientStreams bool
+	serverStreams bool
 }
 
 type httpHandler struct {
