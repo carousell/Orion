914b4685bce6463821a24659a37093075bd93939
Ronald Hsu (hothero) <hothero0705@gmail.com>
2021-01-14 10:36:00 +0800

    Create notifier level types and do logging with correct level (#147)
    * ignore idea folder from Goland
    
    * define notifier own type for severity
    
    * apply the severity in doNotify and fix logger level
    
    * also consider error severity when convert to logger level
    
    * remove RavenSeverity from struct functions

---
+++ utils/errors/notifier/types.go
@@ -0,0 +1,34 @@
+package notifier
+
+// We use string as argument to determine the log level from day one,
+// so it's not a big deal to make breaking changes to force everyone to adopt.
+// It turns out we expose string type constants for users to use and keep severity type as private for now
+type severity string
+
+const (
+	ErrorLevel   = "error"
+	DebugLevel   = "debug"
+	InfoLevel    = "info"
+	WarningLevel = "warning"
+	FatalLevel   = "fatal"
+)
+
+const (
+	errorSeverity   = severity(ErrorLevel)
+	debugSeverity   = severity(DebugLevel)
+	infoSeverity    = severity(InfoLevel)
+	warningSeverity = severity(WarningLevel)
+	fatalSeverity   = severity(FatalLevel)
+)
+
+var (
+	// mainly for mapping string back used
+	levelSeverityMap = map[string]severity{
+		ErrorLevel:   errorSeverity,
+		DebugLevel:   debugSeverity,
+		InfoLevel:    infoSeverity,
+		WarningLevel: warningSeverity,
+		FatalLevel:   fatalSeverity,
+		"critical":   fatalSeverity, // backward capability
+	}
+)
