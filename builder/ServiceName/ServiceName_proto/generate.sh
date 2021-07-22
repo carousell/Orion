2f78283a00aa90ceaec0f06d9cdd0cc2b33436ca
Ronald Hsu (hothero) <hothero0705@gmail.com>
2021-07-23 02:33:21 +0800

    add go module file and upgrade protobuf generation script

--- builder/ServiceName/ServiceName_proto/generate.sh
+++ builder/ServiceName/ServiceName_proto/generate.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 echo "generating proto"
-protoc -I . ServiceName.proto --go_out=plugins=grpc:. --orion_out=.
+protoc -I . ServiceName.proto --go_out=plugins=grpc:$GOPATH/src/. --orion_out=.
+sed -i "" "s/,omitempty//" ServiceName.pb.go
