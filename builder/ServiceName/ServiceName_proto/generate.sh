162f3c5a0f900275a85c20e148d3ec466b18d7aa
Ronald Hsu (hothero) <hothero0705@gmail.com>
2021-07-23 17:30:20 +0800

    Upgrade builder (#156)
    * remove builder/vendor
    
    * add go module file and upgrade protobuf generation script
    
    * update create.sh
    
    * rollback Dockerfile changes
    
    * fix typo

--- builder/ServiceName/ServiceName_proto/generate.sh
+++ builder/ServiceName/ServiceName_proto/generate.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "generating proto"
-protoc -I . ServiceName.proto --go_out=plugins=grpc:. --orion_out=.
+protoc -I . ServiceName.proto --go_out=plugins=grpc:$GOPATH/src/. --orion_out=.
