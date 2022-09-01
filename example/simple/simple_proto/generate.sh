67eab867cdc0e86237770b2a08d349d2b93769f9
Benjamin Heng <benjamin.heng@thecarousell.com>
2022-09-01 15:20:17 +0800

    Update generation script

--- example/simple/simple_proto/generate.sh
+++ example/simple/simple_proto/generate.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-protoc -I . simple.proto --go_out="paths=source_relative:." --go-grpc_out="paths=source_relative:." --orion_out=.
+protoc -I . simple.proto \
+    --go_out="." \
+    --go_opt="paths=source_relative" \
+    --go-grpc_out="." \
+    --go-grpc_opt="paths=source_relative" \
+    --orion_out=.
