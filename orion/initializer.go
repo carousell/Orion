833869abe1eb2c659e32458704b1f25e67f5a691
Achi Chen <achi.chen@thecarousell.com>
2023-03-09 00:52:08 +0800

    Remove errorLoggingInitializer

--- orion/initializer.go
+++ orion/initializer.go
@@ -1,48 +1,6 @@
 package orion
 
-import (
-	"context"
-	"strings"
-
-	"github.com/carousell/Orion/v2/utils/errors/notifier"
-	"github.com/carousell/Orion/v2/utils/log"
-)
-
 var (
 	//DefaultInitializers are the initializers applied by orion as default
-	DefaultInitializers = []Initializer{
-		ErrorLoggingInitializer(),
-	}
+	DefaultInitializers = []Initializer{}
 )
-
-//ErrorLoggingInitializer returns a Initializer implementation for error notifier
-func ErrorLoggingInitializer() Initializer {
-	return &errorLoggingInitializer{}
-}
-
-type errorLoggingInitializer struct{}
-
-func (e *errorLoggingInitializer) Init(svr Server) error {
-	env := svr.GetOrionConfig().Env
-	// environment for error notification
-	notifier.SetEnvironemnt(env)
-
-	// rollbar
-	rToken := svr.GetOrionConfig().RollbarToken
-	if strings.TrimSpace(rToken) != "" {
-		notifier.InitRollbar(rToken, env)
-		log.Debug(context.Background(), "rollbarToken", rToken, "env", env)
-	}
-
-	//sentry
-	sToken := svr.GetOrionConfig().SentryDSN
-	if strings.TrimSpace(sToken) != "" {
-		notifier.InitSentry(sToken)
-		log.Debug(context.Background(), "sentryDSN", rToken, "env", env)
-	}
-	return nil
-}
-
-func (e *errorLoggingInitializer) ReInit(svr Server) error {
-	return e.Init(svr)
-}
