1f569f447481d7c79d1effe84df9cef9eeb077ad
Achi Chen <achi.chen@thecarousell.com>
2023-03-09 13:46:00 +0800

    Move out utils/spanutils package

--- utils/documentation.go
+++ utils/documentation.go
@@ -4,6 +4,5 @@ package utils
 //go:generate godoc2ghmd -ex -file=README.md github.com/carousell/Orion/utils
 //go:generate godoc2ghmd -ex -file=headers/README.md github.com/carousell/Orion/utils/headers
 //go:generate godoc2ghmd -ex -file=listenerutils/README.md github.com/carousell/Orion/utils/listenerutils
-//go:generate godoc2ghmd -ex -file=spanutils/README.md github.com/carousell/Orion/utils/spanutils
 //go:generate godoc2ghmd -ex -file=options/README.md github.com/carousell/Orion/utils/options
 //go:generate godoc2ghmd -ex -file=log/README.md github.com/carousell/Orion/utils/log
