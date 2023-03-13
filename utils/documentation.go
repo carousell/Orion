9252670a5e1e1355d1fc67ff915b1231046e2081
Achi Chen <achi.chen@thecarousell.com>
2023-03-14 01:28:43 +0800

    Remove godoc2ghmd

--- utils/documentation.go
+++ utils/documentation.go
@@ -4,4 +4,3 @@ package utils
 //go:generate godoc2ghmd -ex -file=README.md github.com/carousell/Orion/v2/utils
 //go:generate godoc2ghmd -ex -file=listenerutils/README.md github.com/carousell/Orion/v2/utils/listenerutils
 //go:generate godoc2ghmd -ex -file=options/README.md github.com/carousell/Orion/v2/utils/options
-//go:generate godoc2ghmd -ex -file=log/README.md github.com/carousell/Orion/v2/utils/log
