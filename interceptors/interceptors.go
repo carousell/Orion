08158e9ce0af4118de3ed57daaf453ada8a99609
Achi Chen <achi.chen@thecarousell.com>
2023-03-21 10:04:43 +0800

    Remove gpc_ctxtags, opentracing, prometheus interceptors

--- interceptors/interceptors.go
+++ interceptors/interceptors.go
@@ -1,35 +1,12 @@
 package interceptors
 
 import (
-	"context"
-	"strings"
-
-	grpc_ctxtags "github.com/grpc-ecosystem/go-grpc-middleware/tags"
-	grpc_opentracing "github.com/grpc-ecosystem/go-grpc-middleware/tracing/opentracing"
-	grpc_prometheus "github.com/grpc-ecosystem/go-grpc-prometheus"
 	"google.golang.org/grpc"
 )
-
-var (
-	//FilterMethods is the list of methods that are filtered by default
-	FilterMethods = []string{"Healthcheck", "HealthCheck"}
-)
-
-func filterFromZipkin(ctx context.Context, fullMethodName string) bool {
-	for _, name := range FilterMethods {
-		if strings.Contains(fullMethodName, name) {
-			return false
-		}
-	}
-	return true
-}
 
 //DefaultInterceptors are the set of default interceptors that are applied to all Orion methods
 func DefaultInterceptors() []grpc.UnaryServerInterceptor {
 	return []grpc.UnaryServerInterceptor{
-		grpc_ctxtags.UnaryServerInterceptor(),
-		grpc_opentracing.UnaryServerInterceptor(grpc_opentracing.WithFilterFunc(filterFromZipkin)),
-		grpc_prometheus.UnaryServerInterceptor,
 		ServerErrorInterceptor(),
 		PanicRecoveryInterceptor(),
 	}
@@ -38,9 +15,6 @@ func DefaultInterceptors() []grpc.UnaryServerInterceptor {
 //DefaultStreamInterceptors are the set of default interceptors that should be applied to all Orion streams
 func DefaultStreamInterceptors() []grpc.StreamServerInterceptor {
 	return []grpc.StreamServerInterceptor{
-		grpc_ctxtags.StreamServerInterceptor(),
-		grpc_opentracing.StreamServerInterceptor(),
-		grpc_prometheus.StreamServerInterceptor,
 		ServerErrorStreamInterceptor(),
 	}
 }
